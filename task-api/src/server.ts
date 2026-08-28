84c42d7f8c82bade7ccfae5abcdb63ec3fd4f0b4
gabriels997 <gabriel.s997@outlook.com>
2026-08-28 01:32:47 -0300

    Tasks

--- task-api/src/server.ts
+++ task-api/src/server.ts
@@ -3,11 +3,30 @@ import express from "express";
 const app = express();
 
 app.get("/health", (req, res) => {
+    
     res.json({
         status: "ok",
         message: "Task API is running"
     });
 });
+
+const task = [
+    {
+        id: 1,
+        title: "Estudar Node.js",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "Estudar Azure",
+        completed: false
+    }
+];
+
+app.get("/tasks", (req, res) => {
+    res.json(task);
+});
+
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
